ef6706088d0da1cf98ef6c11c83dd9279051e1a2
ahydorn <adamhydorn@gmail.com>
2019-08-15 14:18:05 -0700

    database update

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
-var mysql = require("mysql");
+var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
   port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  user: 'h76vih75tfs83cbj',
+  password: 'qd78i79o822m0cr6',
+  database: 'dfut9ek6xvin0nmc'
 });
-
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error('error connecting: ' + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
