957e2f43fd7b992f086d00829fe83e804118821b
ahydorn <adamhydorn@gmail.com>
2019-08-15 14:18:23 -0700

    database update

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
+  host: 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306,
   user: 'h76vih75tfs83cbj',
   password: 'qd78i79o822m0cr6',
