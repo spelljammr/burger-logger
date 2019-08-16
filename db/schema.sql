faf4411b682e05174a3977634a72468319262a98
ahydorn <adamhydorn@gmail.com>
2019-08-15 17:38:40 -0700

    update

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,21 @@
 -- Create the burgers_db database --
 CREATE DATABASE burgers_db;
-USE burgers_db;
+-- USE l9vcstm2l9v6al84;
+USE l9vcstm2l9v6al84;
 
 -- Create a burgers table with the required fields --
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	id int NOT NULL
+	AUTO_INCREMENT,
+	burger_name varchar
+	(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-  	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  	PRIMARY KEY(id)
+  	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON
+	UPDATE CURRENT_TIMESTAMP,
+  	dt DATETIME
+	DEFAULT CURRENT_TIMESTAMP ON
+	UPDATE CURRENT_TIMESTAMP,
+  	PRIMARY KEY
+	(id)
 );
